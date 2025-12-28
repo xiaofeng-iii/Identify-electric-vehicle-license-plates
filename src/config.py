352d2008c0646494b4583617464f431670cdc35d
xiaofeng-iii <1517572544@qq.com>
2025-12-28 21:01:43 +0800

    更新模板+更新形态学参数CHAR_MORPH_OPEN_KERNEL = (5,5)-CHAR_MORPH_OPEN_KERNEL = (7, 7)

--- src/config.py
+++ src/config.py
@@ -69,7 +69,7 @@ CHAR_BINARY_INVERT = True         # 是否反转（True=白底黑字变黑底白
 
 # 形态学参数
 CHAR_MORPH_CLOSE_KERNEL = (7, 7)  # 闭运算核大小（连接断裂笔画）
-CHAR_MORPH_OPEN_KERNEL = (5, 5)   # 开运算核大小（去除噪点）
+CHAR_MORPH_OPEN_KERNEL = (7, 7)   # 开运算核大小（去除噪点）
 
 # 字符筛选参数
 CHAR_HEIGHT_RATIO_MIN = 0.2       # 字符高度占车牌高度的最小比例
